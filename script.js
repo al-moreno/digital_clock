4fbadc1ca98b539368325387d8c907aea79a9268
Ana Laura Moreno <almoreno@Anas-MacBook-Pro.local>
2021-11-05 16:01:30 -0500

    css changes. pending conditional logic on numbers to hold two digits

--- script.js
+++ script.js
@@ -3,27 +3,22 @@ const clock = document.querySelector('.clock');
 
 const tick =  () => {
 
-
-
-
-
-
-
-
     const now = new Date();
+
     const hr = now.getHours();
     const min = now.getMinutes();
     const sec = now.getSeconds();
 
+ 
+
     const html = `
     <h4> The current time is: </h6>
-    
     <span>${hr}</span>:
-    <span> ${min}</span>:
-    <span> ${sec}</span>`;
+    <span>${min}</span>:
+    <span>${sec}</span>`;
 
     clock.innerHTML = html;
     
-}
+};
 
 setInterval(tick, 1000);
