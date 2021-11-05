07637366c3f506214f012c9c1a5e3adad0de80b6
Ana Laura Moreno <almoreno@Anas-MacBook-Pro.local>
2021-11-05 16:30:02 -0500

    change to minutes and time format. Pending hour format change

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const clock = document.querySelector('.clock');
 const tick =  () => {
 
     const now = new Date();
+    
+
+    
 
     const hr = now.getHours();
-    const min = now.getMinutes();
-    const sec = now.getSeconds();
+    const min = (now.getMinutes()<10?'0':'') + now.getMinutes();
+    const sec = (now.getSeconds()<10?'0':'') + now.getSeconds();
+
+    // console.log((now.getMinutes()<10?'0':'') + now.getMinutes());
+    //console.log((now.getSeconds()<10?'0':'') + now.getSeconds());
+   
 
  
 
