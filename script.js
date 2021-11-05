f6fd6ec8130c21793934538b9341d26b5a50640d
Ana Laura Moreno <almoreno@Anas-MacBook-Pro.local>
2021-11-05 12:06:16 -0500

    changes due to security of cdn for fns

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const clock = document.querySelector('.clock');
 const tick =  () => {
 
     const now = new Date();
-    const fns = dateFns.format(now, 'dddd Do MMMM YYYY')
+   
 
     const hr = now.getHours();
     const min = now.getMinutes();
     const sec = now.getSeconds();
 
     const html = `
-    <h4> The current day and time is: </h6>
-    <h6> ${fns} </h4>
+    <h4> The current time is: </h6>
+    
     <span>${hr}</span>:
     <span> ${min}</span>:
     <span> ${sec}</span>`;
