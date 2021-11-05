72f28c6ebcf85aec3af5b37f7bebb601aa5f47fe
Ana Laura Moreno <almoreno@Anas-MacBook-Pro.local>
2021-11-05 16:49:17 -0500

    format for hour still pending. Adjusted min and sec

--- script.js
+++ script.js
@@ -8,15 +8,11 @@ const tick =  () => {
 
     
 
-    const hr = now.getHours();
+    const hr = (now.getHours() == 0?'12':now.getHours() >12? now.getHours() -12: now.getHoures());
     const min = (now.getMinutes()<10?'0':'') + now.getMinutes();
     const sec = (now.getSeconds()<10?'0':'') + now.getSeconds();
-
-    // console.log((now.getMinutes()<10?'0':'') + now.getMinutes());
-    //console.log((now.getSeconds()<10?'0':'') + now.getSeconds());
    
 
- 
 
     const html = `
     <h4> The current time is: </h6>
